1e1daccd4dd3d09bfeacc01e7590b759f5245ff2
Balchand Dangi <dangibalchand935@gmail.in>
2024-09-27 15:17:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function redirect1() {
+    window.location.href = "signup.html";
+
+}
+
+function redirect2() {
+    window.location.href = "signin.html";
+    confirm('Have you completed registration ?')
+}
+
+function redirect3() {
+    window.location.href = 'index.html';
+    alert('Registration has completed successfuly.');
+}
+
+function redirect4() {
+    window.location.href = "main.html";
+    alert('Congratulations you are successfully entered.')
+}
